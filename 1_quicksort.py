ff23877666d45fead8828d0d49c68d4965c3acce
Sanket-C-Gaikwad <84791915+Sanket-C-Gaikwad@users.noreply.github.com>
2024-08-14 12:35:02 +0200

    Create 1_quicksort.py

---
+++ 1_quicksort.py
@@ -0,0 +1,28 @@
+
+def quicksort(arr, left, right):
+    if left < right:
+        pivot_index = part(arr, left, right)
+        quicksort(arr, left, pivot_index - 1)
+        quicksort(arr, pivot_index + 1, right)
+
+    
+
+def part(arr, left, right):    
+    
+    i = left - 1
+    pivot = arr[right]
+    
+    for j in range(left, right):
+        
+        if arr[j] <= pivot:
+            i +=1
+            arr[i], arr[j] = arr[j], arr[i]
+    
+    arr[i+1], arr[right] = arr[right], arr[i+1]
+    
+    return i+1
+    
+arr = [2,3,5,66,7,8,2]
+print(arr)
+quicksort(arr, 0, len(arr)-1)
+print(arr)
